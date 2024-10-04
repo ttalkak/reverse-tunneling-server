0982b70cf13b05d07d0d394c1c9dbaffe29d28f5
sunsuking <sunsuking@gmail.com>
2024-10-04 09:10:00 +0900

    디비명 수정

--- core/base.py
+++ core/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@localhost:5432/pgrokd"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@localhost:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
